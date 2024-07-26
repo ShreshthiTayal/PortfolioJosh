fa36ce1e6c194aef49600fce004da739c1d8f77a
ShreshthiTayal <shreshthitayal@gmail.com>
2024-07-27 00:03:02 +0530

    add skill form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import Navbar from './components/Navbar';
 import ContactForm from './components/ContactForm';
 import ContactInfo from './components/ContactInfo';
@@ -6,6 +6,7 @@ import Hero from './components/Hero';
 import Footer from './components/Footer';
 import Projects from './components/Projects';
 import Skills from './components/Skills'
+import AddSkillForm from './components/AddSkillForm';
 const App = () => {
   return (
     <div>
@@ -13,6 +14,7 @@ const App = () => {
       <Hero/>
       <Projects/>
       <Skills/>
+      <AddSkillForm/>
       <div className='flex  bg-[#F0F0F6] justify-around'>   <ContactForm/>
       <ContactInfo/>
       </div>
