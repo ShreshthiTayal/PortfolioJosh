3ca8ac166a70ef805c74bd548e259b1bf1d240be
ShreshthiTayal <shreshthitayal@gmail.com>
2024-07-27 17:02:02 +0530

    homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,12 @@ import ContactForm from './ContactForm';
 import ContactInfo from './ContactInfo';
 import Hero from './Hero';
 import Projects from './Projects';
-import Skills from './Skills';
 import Recommendations from './Recommendations';
 const App = () => {
   return (
     <div>
       <Hero/>
       <Projects/>
-      <Skills/>
       <Recommendations/>
       <div className='flex  bg-[#F0F0F6] justify-around'>   <ContactForm/>
       <ContactInfo/>
